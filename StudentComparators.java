2badbb0225248df51394d2e0a00408fa5802f21a
Priya <preeyaahh26@gmail.com>
2026-03-02 20:39:24 +0530

    Update StudentComparators.java

--- StudentComparators.java
+++ StudentComparators.java
@@ -2,15 +2,9 @@ import java.util.Comparator;
 
 public class StudentComparators {
 
-    public static Comparator<Student> byName = new Comparator<Student>() {
-        public int compare(Student s1, Student s2) {
-            return s1.getName().compareToIgnoreCase(s2.getName());
-        }
-    };
+    public static Comparator<Student> byName =
+            (s1, s2) -> s1.getName().compareToIgnoreCase(s2.getName());
 
-    public static Comparator<Student> byMarks = new Comparator<Student>() {
-        public int compare(Student s1, Student s2) {
-            return s2.getMarks() - s1.getMarks(); // descending
-        }
-    };
+    public static Comparator<Student> byMarks =
+            (s1, s2) -> Integer.compare(s2.getMarks(), s1.getMarks());
 }
